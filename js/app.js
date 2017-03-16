42127dd4c42690f510f3ba5c90af2d03004d8141
Matheus de Quadros <matheus.quadros@catolicasc.org.br>
2017-03-15 21:53:17 -0300

    Testes com validação das formas
    Foi adicionado, somente para um quadrado, a validação se ele sobrepoem
    um elemento.

--- js/app.js
+++ js/app.js
@@ -1,34 +1,73 @@
+ var endGame = false, isIn = false;
+
+ var tangram = {
+    trianguloGrande1: {
+      color : '',
+      status : false
+    },
+    trianguloGrande2: {
+      color : '',
+      status : false
+    },
+    trianguloMedio:  {
+      color : '',
+      status : false
+    },
+    trianguloPequeno1: {
+      color : '',
+      status : false
+    },
+    trianguloPequeno2:  {
+      color : '',
+      status : false
+    },
+    quadrado:  {
+      color : '',
+      status : false
+    },
+    paralelogramo:  {
+      color : '',
+      status : false
+    },
+ };
+
+
  function init() {
  
     // Initialise our object
-    obj = {x:50, y:50, w:70, h:70};
+    obj = {x:300, y:50, w:70, h:70};
+    model = {x:50, y:50, w:70, h:70};
+
     canvas = document.getElementById("canvas");
-    console.log(canvas);
  
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
  
-    // Add eventlistener to canvas
     canvas.addEventListener('touchmove', function() {
-      //Assume only one touch/only process one touch even if there's more
       var touch = event.targetTouches[0];
+
+      if(endGame === true) return;
       
-      // Is touch close enough to our object?
       if(detectHit(obj.x, obj.y, touch.pageX, touch.pageY, obj.w, obj.h)) {
-        // Assign new coordinates to our object
         obj.x = touch.pageX;
         obj.y = touch.pageY;
- 
-        // Redraw the canvas
         draw();
       }
+
+      if(obj.x > model.x-3 && obj.x < model.x+3 && obj.y > model.y-3 && obj.y < model.y+3){
+        isIn = true;
+      }else{
+        isIn = false;
+      }
+
+      console.log(isIn);
+
       event.preventDefault();
     }, false);
     draw();
   }
 
   function detectHit(x1,y1,x2,y2,w,h) {
-  //Very simple detection here
   if(x2-x1>w) return false;
   if(y2-y1>h) return false;
   return true;
@@ -38,15 +77,24 @@
   function draw() {
     canvas = document.getElementById("canvas");
     var ctx = canvas.getContext('2d');
+   
+
  
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //model.clearRect(0, 0, canvas.width, canvas.height);
  
+    ctx.fillStyle = 'gray';
+    ctx.fillRect(model.x, model.y, model.w, model.h);
+
     ctx.fillStyle = 'blue';
- 
-    // Draw our object in its new position
     ctx.fillRect(obj.x, obj.y, obj.w, obj.h);
+
+    
+    //model.clearRect(0, 0, canvas.width, canvas.height);
+ 
   }
+
 TouchEmulator();
   init();
   
