27f7516b4f043406f3cae1156d3e97b63abcc7f8
Matheus de Quadros <matheus.quadros@catolicasc.org.br>
2017-05-03 20:34:44 -0300

    adição da biblioteca phaser.js

--- js/app.js
+++ js/app.js
@@ -1,98 +1,52 @@
-var endGame = false, isIn = false;
+var URLfiguras = ['assets/gato/trian1.png', 'assets/gato/trian2.png', 'assets/gato/quad1.png', 'assets/gato/trian3.png', 'assets/gato/trian4.png', 'assets/gato/trian5.png', 'assets/gato/paralelo1.png'];
+var  nomesFiguras = ['figura0', 'figura1', 'figura2', 'figura3', 'figura4', 'figura5', 'figura6'];
+var figuras = new Array(URLfiguras.length);
+var figurasCopy = new Array(URLfiguras.length);
 
-var tangram = {
-  trianguloGrande1: {
-    color: '',
-    status: false
-  },
-  trianguloGrande2: {
-    color: '',
-    status: false
-  },
-  trianguloMedio: {
-    color: '',
-    status: false
-  },
-  trianguloPequeno1: {
-    color: '',
-    status: false
-  },
-  trianguloPequeno2: {
-    color: '',
-    status: false
-  },
-  quadrado: {
-    color: '',
-    status: false
-  },
-  paralelogramo: {
-    color: '',
-    status: false
-  },
-};
+var  anchorsetTo1 = [1.6, 0.6, 0.8, 1, 0, 0.3, -0.7];//Eixo x figuraEncaixe
+var  anchorsetTo2 = [4, 4, 3.5, 2, 1.4, 1.3, 1.6];//Eixo y figuraEncaixe
+var anchorX = [7, 7, 3.6, 5, 3.5, 2.6, 2.6]; //Eixo x figuraAEncaixar
+var dimensaoSprite = [20, 100, 230, 340, 460, 585, 790]; //Eixo y figuraAEncaixar
 
-function init() {
-
-  // Initialise our object
-  obj = { x: 300, y: 50, w: 70, h: 70 };
-  model = { x: 50, y: 50, w: 70, h: 70 };
-
-  canvas = document.getElementById("canvas");
-
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  canvas.addEventListener('touchmove', function () {
-    var touch = event.targetTouches[0];
-
-    if (endGame === true) return;
-
-    if (detectHit(obj.x, obj.y, touch.pageX, touch.pageY, obj.w, obj.h)) {
-      obj.x = touch.pageX;
-      obj.y = touch.pageY;
-      draw();
+var game = new Phaser.Game(window.innerWidth-15, window.innerHeight -20, Phaser.AUTO, 'tangran', {preload: preload, create: create});
+    
+function preload(){    
+    for (var i = 0; i < figuras.length; i++){
+        game.load.image(nomesFiguras[i], URLfiguras[i]);
     }
+}
+    
+function create(){    
+    
+    for (var i = 0; i < figuras.length; i++){ 
 
-    if (obj.x > model.x - 3 && obj.x < model.x + 3 && obj.y > model.y - 3 && obj.y < model.y + 3) {
-      isIn = true;
-    } else {
-      isIn = false;
+     //figura encaixe
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        figuras[i] = game.add.sprite(game.world.centerX, game.world.height, nomesFiguras[i]);
+        figuras[i].anchor.setTo(anchorsetTo1[i], anchorsetTo2[i]); //Disposição figuraEncaixe
+        game.physics.arcade.enable(figuras[i]);
+        figuras[i].tint= 0x00beff; //Cor figuraEncaixe
+
+        //figura a encaixar  
+        figurasCopy[i] = game.add.sprite(game.world.centerX, dimensaoSprite[i], figuras[i].key, figuras[i].frame); //DimensãoSprite
+        figurasCopy[i].anchor.x = anchorX[i]; //Eixo x figuraAEncaixar
+        game.physics.arcade.enable(figurasCopy[i]);
+        figurasCopy[i].inputEnabled = true;
+        figurasCopy[i].input.enableDrag();
+        figurasCopy[i].originalPosition = figurasCopy[i].position.clone();
+        figurasCopy[i].events.onDragStop.add(function(currentSprite){
+          stopDrag(currentSprite, figuras[i]);
+        }, this);  
+        
     }
-
-    console.log(isIn);
-
-    event.preventDefault();
-  }, false);
-  draw();
-}
-
-function detectHit(x1, y1, x2, y2, w, h) {
-  if (x2 - x1 > w) return false;
-  if (y2 - y1 > h) return false;
-  return true;
-}
-
-
-function draw() {
-  canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext('2d');
-
-
-
-  // Clear the canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  //model.clearRect(0, 0, canvas.width, canvas.height);
-
-  ctx.fillStyle = 'gray';
-  ctx.fillRect(model.x, model.y, model.w, model.h);
-
-  ctx.fillStyle = 'blue';
-  ctx.fillRect(obj.x, obj.y, obj.w, obj.h);
-
-
-  //model.clearRect(0, 0, canvas.width, canvas.height);
-
-}
-
-TouchEmulator();
-init();
+    
+  }
+  
+function stopDrag(currentSprite, endSprite){
+    if (!game.physics.arcade.overlap(currentSprite, endSprite, function() {
+    currentSprite.input.draggable = false;
+    currentSprite.position.copyFrom(endSprite.position); 
+    currentSprite.anchor.setTo(endSprite.anchor.x, endSprite.anchor.y); 
+  })) { currentSprite.position.copyFrom(currentSprite.originalPosition);
+  }
+  }
