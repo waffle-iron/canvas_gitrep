0d9912dab1f15abf343ca40c01e9849693663385
Matheus de Quadros <matheus.quadros@catolicasc.org.br>
2017-05-08 21:32:41 -0300

    Validação do tangran formado

--- js/app.js
+++ js/app.js
@@ -1,67 +1,115 @@
-var URLfiguras = ['assets/gato/trian1.png', 'assets/gato/trian2.png', 'assets/gato/quad1.png', 'assets/gato/trian3.png', 'assets/gato/trian4.png', 'assets/gato/trian5.png', 'assets/gato/paralelo1.png'];
-var  nomesFiguras = ['figura0', 'figura1', 'figura2', 'figura3', 'figura4', 'figura5', 'figura6'];
-var figuras = new Array(URLfiguras.length);
-var figurasCopy = new Array(URLfiguras.length);
-
 var  anchorsetTo1 = [1.6, 0.6, 0.8, 1, 0, 0.3, -0.7];//Eixo x figuraEncaixe
 var  anchorsetTo2 = [4, 4, 3.5, 2, 1.4, 1.3, 1.6];//Eixo y figuraEncaixe
 var anchorX = [7, 7, 3.6, 5, 3.5, 2.6, 2.6]; //Eixo x figuraAEncaixar
-var dimensaoSprite = [20, 100, 230, 340, 460, 585, 790]; //Eixo y figuraAEncaixar
+var dimensaoSprite = [20, 100, 230, 340, 460, 585, 0]; //Eixo y figuraAEncaixar
+var tangran, interval, tempo;
+
+function tangramItem(figureId, url){
+    this.figureName = "figura"+figureId,
+    this.url = "assets/gato/"+url+".png";
+    this.status = false,
+    this.object = {},
+    this.objectForm = {}
+}
+
+function setTangran(){
+    tangran = [
+        new tangramItem('0', "trian1"),
+        new tangramItem('1', "trian2"),
+        new tangramItem('2', "quad1"),
+        new tangramItem('3', "trian3"),
+        new tangramItem('4', "trian4"),
+        new tangramItem('5', "trian5"),
+        new tangramItem('6', "paralelo1"),
+    ];
+};
+
+setTangran();
 
 var game = new Phaser.Game(window.innerWidth-15, window.innerHeight -20, Phaser.AUTO, 'tangran', {preload: preload, create: create});
     
-function preload(){    
-    for (var i = 0; i < figuras.length; i++){
-        game.load.image(nomesFiguras[i], URLfiguras[i]);
-    }
+function preload(){ 
+    game.stage.backgroundColor = "#e0e4f1";   
+    tangran.forEach(function(item){
+         game.load.image(item.figureName, item.url);
+    });
 }
     
-function create(){    
-    for (var j = 0; j < figuras.length; j++){
-        //Cria molde do tangran
+function create(){
+    tangran.forEach(function(item, i){
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        figuras[j] = game.add.sprite(game.world.centerX, game.world.height, nomesFiguras[j]);
-        figuras[j].anchor.setTo(anchorsetTo1[j], anchorsetTo2[j]); //Disposição figuraEncaixe
-        game.physics.arcade.enable(figuras[j]);
-        figuras[j].tint= 0x00beff; //Cor figuraEncaixe
-    }
-    
-    for (var i = 0; i < figuras.length; i++){ 
-        //Cria as peças do jogo
-        figurasCopy[i] = game.add.sprite(game.world.centerX, dimensaoSprite[i], figuras[i].key, figuras[i].frame); //DimensãoSprite
-        figurasCopy[i].anchor.x = anchorX[i]; //Eixo x figuraAEncaixar
-        game.physics.arcade.enable(figurasCopy[i]);
-        figurasCopy[i].inputEnabled = true;
-        figurasCopy[i].input.enableDrag();
-        figurasCopy[i].originalPosition = figurasCopy[i].position.clone();
+        item.object = game.add.sprite(game.world.centerX, game.world.height, item.figureName);
+        item.object.anchor.setTo(anchorsetTo1[i], anchorsetTo2[i]); //Disposição figuraEncaixe
+        game.physics.arcade.enable(item.object);
+        item.object.tint= 0x00beff; //Cor figuraEncaixe
+    });
 
-        figurasCopy[i].events.onDragStop.add(function(currentSprite){
-          stopDrag(currentSprite, figuras[nomesFiguras.indexOf(currentSprite.key)]);
-        }, this);  
-    }
+    tangran.forEach(function(item, i){
+        item.objectForm = game.add.sprite(game.world.centerX, dimensaoSprite[i], item.object.key, item.object.frame); //DimensãoSprite
+        item.objectForm.anchor.x = anchorX[i]; //Eixo x figuraAEncaixar
+        game.physics.arcade.enable(item.objectForm);
+        item.objectForm.inputEnabled = true;
+        item.objectForm.input.enableDrag();
+        item.objectForm.originalPosition = item.objectForm.position.clone();
+        item.objectForm.tint = Phaser.Color.getRandomColor(50, 255, 255);
+
+        item.objectForm.events.onDragStop.add(function(currentSprite){
+          var currentItem = tangran.filter(function(element){return element.figureName === currentSprite.key}).shift();
+          stopDrag(currentSprite, currentItem.object, tangran.indexOf(currentItem));
+        }, this);
+    });
+
+}
+
+function win(){
+    var youWon = true
+    tangran.forEach(function(item){
+        if(item.status === false){
+            youWon = false;
+        }
+    });
+    return youWon;
 }
   
-  
-function stopDrag(currentSprite, endSprite){
+function stopDrag(currentSprite, endSprite, itemTangramIndex){
     if (!game.physics.arcade.overlap(currentSprite, endSprite, function() {
         // Codigo responsavel pelo reposicionamento das peças.
     //currentSprite.input.draggable = false;
     //currentSprite.position.copyFrom(endSprite.position); 
     //currentSprite.anchor.setTo(endSprite.anchor.x, endSprite.anchor.y); 
+    
   })) 
   { 
-      //currentSprite.position.copyFrom(currentSprite.originalPosition);
+    tangran[itemTangramIndex].status = false;
+ 
+  }else{
+    tangran[itemTangramIndex].status = true;
+    if(win()){
+        alert("Você ganhou!\n Tempo:"+tempo);
+    }
   }
-  }
+  
+}
 
-(function timer(){
+
+
+function timer(){
   var start = new Date();
   start.setHours(0,0,0,0);
-  var tempo;
-  var interval = setInterval(function(){
+  if(interval !== undefined){
+      clearInterval(interval);
+  }
+    interval = setInterval(function(){
     tempo = (start.getMinutes()<10?'0':'') + start.getMinutes() +" : "+ (start.getSeconds()<10?'0':'') + start.getSeconds();
     start.setSeconds(start.getSeconds() + 1);
     document.querySelector('.teste').innerHTML = '<p>'+tempo+'</p>';
   }, 1000);
-})();
+};
+
+timer();
+
+function reset (){
+    timer();
+    //setTangran();
+};
 
